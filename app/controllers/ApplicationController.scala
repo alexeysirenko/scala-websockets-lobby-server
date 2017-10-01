f3af37e1ea2d04052bc3fdffc31bee9d587d0d81
Alexey Sirenko <drunk.pacifist@gmail.com>
2017-10-01 19:23:27 +0300

    Remove an ability to check an Origin value of the request due to unnecessity

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -6,13 +6,17 @@ import javax.inject._
 import actors.{AuthorizationActor, LobbyClientActor, TablesActor}
 import akka.actor.ActorSystem
 import akka.stream.Materializer
+import message.LobbyMessage
 import play.api._
 import play.api.libs.json.JsValue
 import play.api.libs.streams.ActorFlow
+import play.api.mvc.WebSocket.MessageFlowTransformer
 import play.api.mvc._
 
-import scala.concurrent.Future
-
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
 @Singleton
 class ApplicationController @Inject()(cc: ControllerComponents)(implicit system: ActorSystem, mat: Materializer)
   extends AbstractController(cc) {
@@ -20,59 +24,13 @@ class ApplicationController @Inject()(cc: ControllerComponents)(implicit system:
   private val authorizationActor = system.actorOf(AuthorizationActor.props(), "authorization-actor")
   private val tablesActor = system.actorOf(TablesActor.props(), "tables-actor")
 
-  def lobby: WebSocket = WebSocket.acceptOrResult[JsValue, JsValue] {
-    case rh if sameOriginCheck(rh) =>
-      Future.successful {
-        Right(ActorFlow.actorRef { out =>
-          LobbyClientActor.props(out, authorizationActor, tablesActor)
-        })
-      }
-    case rejected =>
-      Logger.error(s"Request $rejected failed same origin check")
-      Future.successful {
-        Left(Forbidden("forbidden"))
-      }
+  def lobby: WebSocket = WebSocket.accept[JsValue, JsValue] { request =>
+    ActorFlow.actorRef { out =>
+      LobbyClientActor.props(out, authorizationActor, tablesActor)
+    }
   }
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
-
-  /**
-    * Checks that the WebSocket comes from the same origin.  This is necessary to protect
-    * against Cross-Site WebSocket Hijacking as WebSocket does not implement Same Origin Policy.
-    *
-    * See https://tools.ietf.org/html/rfc6455#section-1.3 and
-    * http://blog.dewhurstsecurity.com/2013/08/30/security-testing-html5-websockets.html
-    */
-  private def sameOriginCheck(implicit rh: RequestHeader): Boolean = {
-    // The Origin header is the domain the request originates from.
-    // https://tools.ietf.org/html/rfc6454#section-7
-    Logger.debug("Checking the ORIGIN ")
-    rh.headers.get("Origin") match {
-      case Some(originValue) if originMatches(originValue) =>
-        Logger.debug(s"originCheck: originValue = $originValue")
-        true
-      case Some(badOrigin) =>
-        Logger.error(s"originCheck: rejecting request because Origin header value ${badOrigin} is not in the same origin")
-        false
-      case None =>
-        Logger.error("originCheck: rejecting request because no Origin header found")
-        false
-    }
-  }
-
-  /**
-    * Returns true if the value of the Origin header contains an acceptable value.
-    */
-  private def originMatches(origin: String): Boolean = {
-    try {
-      val url = new URL(origin)
-      url.getHost == "localhost" &&
-        (url.getPort match { case 9000 | 19001 => true; case _ => false })
-    } catch {
-      case e: Exception => false
-    }
-  }
-
 }
